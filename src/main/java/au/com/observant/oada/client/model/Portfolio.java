833d02d593a7502c6d908714097abbf78f2d8322
Jonathan Harvey <observer@Observers-MacBook-Pro.local>
2015-04-07 09:44:07 +1000

    Adding Portfolio Endpoint Example

---
+++ src/main/java/au/com/observant/oada/client/model/Portfolio.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2003-2015 by Observant Pty. Ltd. All rights reserved.
+ * Redistribution or reproduction of part or all of the contents of
+ * this file in any form is prohibited.
+ */
+package au.com.observant.oada.client.model;
+
+/**
+ *
+ */
+public class Portfolio {
+
+    private final String id;
+    private final String name;
+
+    /**
+     * @param id
+     * @param name
+     */
+    public Portfolio(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * Returns id.
+     *
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Returns name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Portfolio other = (Portfolio) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "Portfolio [id=" + id + ", name=" + name + "]";
+    }
+}
